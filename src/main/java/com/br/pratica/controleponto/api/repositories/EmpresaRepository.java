3d957e8deb9ebd27aa6098e7f1afe7db484b972b
Arthur Lima <arthurcantanhede@hotmail.com>
2020-03-01 13:38:43 -0300

    Implementação dos Repositorios

--- src/main/java/com/br/pratica/controleponto/api/repositories/EmpresaRepository.java
+++ src/main/java/com/br/pratica/controleponto/api/repositories/EmpresaRepository.java
@@ -1,10 +1,12 @@
 package com.br.pratica.controleponto.api.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.br.pratica.controleponto.api.entities.Empresa;
 
 public interface EmpresaRepository extends JpaRepository<Empresa, Long>{
 
+	@Transactional(readOnly = true)
 	Empresa findByCnpj(String cnpj);
 }
